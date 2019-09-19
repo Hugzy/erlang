c6956c3bdc5bc8bc630d89ca068b0ef1d49e6caa
frederik <frede.madsen3@gmail.com>
2019-09-19 17:52:20 +0200

    merge folders from pre and post installation of windows

---
+++ 4-shop/shop.erl
@@ -0,0 +1,19 @@
+-module(shop).
+-export([test/0, cost/1, total/1]).
+
+test() ->
+    5 = cost(oranges),
+    8 = cost(newspaper),
+    2 = cost(apples),
+    9 = cost(pears),
+    7 = cost(milk),
+    ok.
+
+total([{What, N}|T]) -> shop:cost(What) * N + total(T);
+total([]) -> 0.
+
+cost(oranges)   -> 5;
+cost(newspaper) -> 8;
+cost(apples)    -> 2;
+cost(pears)     -> 9;
+cost(milk)      -> 7.
