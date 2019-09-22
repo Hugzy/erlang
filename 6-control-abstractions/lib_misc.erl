e7a6fc8585cb22974d4501eabdeca0cce1a04c06
frederik <frede.madsen3@gmail.com>
2019-09-22 21:09:05 +0200

    6

--- 6-control-abstractions/lib_misc.erl
+++ 6-control-abstractions/lib_misc.erl
@@ -1,5 +1,11 @@
 -module(lib_misc).
--export([for/3]).
+-export([sum/1, map/2, for/3]).
 
 for(Max, Max, F) -> [F(Max)];
 for(I, Max, F)   -> [F(I)|for(I+2, Max, F)].
+
+sum([H|T])  -> H + sum(T);
+sum([])     -> 0.
+
+map(_, [])         -> [];
+map(F, [H|T])   -> [F(H) | map(F,T)].
