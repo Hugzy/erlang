1ddbd99761a464019e91435c2ca85638ebecf095
frederik <frede.madsen3@gmail.com>
2019-10-07 08:03:28 +0200

    finished sequential error handling, starting on binaries and manipulating raw data with patternmatching

--- 9-errorhandling-sequential-programs/raising_exceptions.erl
+++ 9-errorhandling-sequential-programs/raising_exceptions.erl
@@ -1,5 +1,5 @@
 -module(raising_exceptions).
--export([try_exit/0, try_throw/0, try_error/0]).
+-export([try_exit/0, try_throw/0, try_error/0, read/1]).
 
 try_exit() ->
     exit({error, "the cake has exited, contact are 51"}).
@@ -9,3 +9,26 @@ try_throw() ->
 
 try_error() ->
     error({error, "the cake had an error, contact are 51"}).
+
+read(File) -> 
+    case file:read_file(File) of
+        {ok, Bin} ->
+            Bin;
+        {error, Why} -> 
+            error({error, Why, "You probably entered an invalid file name, either the file doesn't exist at all or you mispelled its name"})
+    end.
+
+generate_exception(1) -> a;
+generate_exception(2) -> throw(a);
+generate_exception(3) -> exit(a);
+generate_exception(4) -> {'EXIT', a};
+generate_exception(5) -> error(a).
+
+catcher(N) ->
+    try generate_exception(N) of
+        Val -> {N, normal, Val}
+    catch
+        throw:A -> {N, caught, thrown, A};
+        exit:B -> {N, caught, exited, B};
+        error:C -> {N, caught, error, C}
+    end.
