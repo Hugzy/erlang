7e68c09c1495cfc2a51e580ef57deec49960cd9c
frederik <frede.madsen3@gmail.com>
2019-10-14 22:02:05 +0200

    almost done with 2nd excersice

--- 10-binaries/excersices.erl
+++ 10-binaries/excersices.erl
@@ -1,6 +1,16 @@
 -module(excersices).
--export([rev/2]).
+-export([rev/2, term_to_packet/1]).
 
 rev (<<>>, Acc) -> Acc;
+% H:1/binary -> Value:Size/TypeSpecifierList, Rest/binary -> Value/TypeSpecifierList
 rev (<<H:1/binary, Rest/binary>>, Acc) ->
+    erlang:display(H),
+    erlang:display(Rest),
     rev(Rest, <<H/binary, Acc/binary>>).
+
+term_to_packet(Term) ->
+    Bin = term_to_binary(Term),
+    Size = byte_size(Bin),
+    erlang:display("Binary", Bin),
+    erlang:display("Size", Size),
+    <<Size:32/binary, Bin/binary>>.
--- 5-Funs/funs.erl
+++ 5-Funs/funs.erl
@@ -3,9 +3,3 @@
 
 test() ->
     ok.
-
-cost(oranges)   -> 5;
-cost(newspaper) -> 8;
-cost(apples)    -> 2;
-cost(pears)     -> 9;
-cost(milk)      -> 7.
