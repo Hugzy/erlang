bcde808ac0291e8cf409859dc882f45c1754f8d5
frederik <frede.madsen3@gmail.com>
2019-10-19 01:22:21 +0200

    finished bit assignment

--- 10-binaries/excersices.erl
+++ 10-binaries/excersices.erl
@@ -1,18 +1,24 @@
 -module(excersices).
--export([rev/2, term_to_packet/1, packet_to_term/1]).
+-export([rev/2, term_to_packet/1, packet_to_term/1, rev_binary/1, test/0]).
+
+test() ->
+    Packet1 = term_to_packet(hello),
+    Packet2 = term_to_packet(fox),
+    <<0,0,0,9,131,100,0,5,104,101,108,108,111>> = Packet1,
+    <<0,0,0,7,131,100,0,3,102,111,120>> = Packet2,
+    hello = packet_to_term(Packet1),
+    fox = packet_to_term(Packet2),
+    ok.
+
 
 rev (<<>>, Acc) -> Acc;
 % H:1/binary -> Value:Size/TypeSpecifierList, Rest/binary -> Value/TypeSpecifierList
 rev (<<H:1/binary, Rest/binary>>, Acc) ->
-    erlang:display(H),
-    erlang:display(Rest),
     rev(Rest, <<H/binary, Acc/binary>>).
 
 term_to_packet(Term) ->
     Bin = term_to_binary(Term),
     Size = byte_size(Bin),
-    erlang:display(Bin),
-    erlang:display(Size),
     <<Size:32, Bin/binary>>.
 
 packet_to_term(Packet) ->
@@ -20,7 +26,8 @@ packet_to_term(Packet) ->
     {<<Size:32>>,_} = Res,
     {_, <<Binary:Size/binary>>} = Res,
     Term = binary_to_term(Binary),
-    erlang:display(Term),
     Term.
 
-
+rev_binary(Binary) ->
+    <<A:1, B:1, C:1, D:1, E:1, F:1, G:1, H:1>> = Binary,
+    <<H:1, G:1, F:1, E:1, D:1, C:1, B:1, A:1>>.
