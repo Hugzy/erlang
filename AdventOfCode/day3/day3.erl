b44296bf62b35ee8b775339ec3eb2491ecd0f1df
frederik <frede.madsen3@gmail.com>
2019-12-11 23:20:47 +0100

    progress, can calculate if two lines intersect and can also map all the calculated endpoints to lines, now to check for each if it interesects with a line from the other list

--- AdventOfCode/day3/day3.erl
+++ AdventOfCode/day3/day3.erl
@@ -11,6 +11,9 @@ test_data_1() ->
 test_data_2() ->
     "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51,U98,R91,D20,R16,D67,R40,U7,R15,U6,R7".
 
+get_data(Data) ->
+    string:tokens(Data, ",").
+
 print({list, List}) ->
     io:fwrite("list: ~128p~n", [List]).
 
@@ -30,13 +33,9 @@ calculate('D', Length, {point, X1, Y1}) ->
     {point, X1, Y1-Length}.
 
 onLine(Line, Point) ->
-   %if(p.x <= max(l1.p1.x, l1.p2.x) && p.x <= min(l1.p1.x, l1.p2.x) &&
-   %   (p.y <= max(l1.p1.y, l1.p2.y) && p.y <= min(l1.p1.y, l1.p2.y)))
-   %   return true;
-   %return false;
-
-   
-
+    {line, {point, Lx1, Ly1}, {point, Lx2, Ly2}} = Line,
+    {point, Px, Py} = Point,
+    ((Px =< max(Lx1, Lx2)) and (Px =< min(Lx1, Lx2)) and ((Py =< max(Ly1, Ly2)) and (Py =< min(Ly1, Ly2)))).
 
 direction_identifier(Identifier) when Identifier == 0 ->
     0;
@@ -53,7 +52,38 @@ direction(A, B, C) ->
     %int val = (b.y-a.y)*(c.x-b.x)-(b.x-a.x)*(c.y-b.y);
     Val = (By-Ay)*(Cx-Bx)-(Bx-Ax)*(Cy-By),
 
+    % Get the direction identifier based on the value calculated
     direction_identifier(Val).
+
+comput_intersection(Dir1, Dir2, Dir3, Dir4) when Dir1 /= Dir2 andalso Dir3 /= Dir4 ->
+    true.
+
+isOnLine(Dir1, OnLine) when Dir1 == 0 andalso OnLine ->
+    true;
+isOnLine(Dir2, OnLine) when Dir2 == 0 andalso OnLine ->
+    true;
+isOnLine(Dir3, OnLine) when Dir3 == 0 andalso OnLine ->
+    true;
+isOnLine(Dir4, OnLine) when Dir4 == 0 andalso OnLine ->
+    true;
+isOnLine(_, _) ->
+    false.
+
+isIntersect(L1, L2) ->
+    {line, P1 , P2} = L1,
+    {line, P3, P4} = L2,
+    Dir1 = direction(P1, P2, P3),
+    Dir2 = direction(P1, P2, P4),
+    Dir3 = direction(P3, P4, P1),
+    Dir4 = direction(P3, P4, P2),
+
+    comput_intersection(Dir1, Dir2, Dir3, Dir4) or isOnLine(Dir1, (onLine(L1, P3))) or isOnLine(Dir2, (onLine(L1, P4))) or isOnLine(Dir3, (onLine(L2, P1))) or isOnLine(Dir4, (onLine(L2, P2))).
+
+group([H|T]) when length(T) > 0 ->
+    [H1|_] = T,
+    [{line, H, H1}|group(T)];
+group(_) ->
+    [].
 
 
 test() ->
@@ -66,16 +96,19 @@ test() ->
     %Results2 = loop(List2, start()),
     %print({list, Results2}),
 
-    P1 = {point, 0, 0},
-    P2 = {point, 5, 5},
-    P3 = {point, 2,-10},
-    P4 = {point, 3, 10},
-    Dir1 = direction(P1, P2, P3),
-    Dir2 = direction(P1, P2, P4),
-    Dir3 = direction(P3, P4, P1),
-    Dir4 = direction(P3, P4, P2),
+    Results1 = loop(convert(get_data(test_data_1())), start()),
+    Results2 = loop(convert(get_data(test_data_2())), start()),
+    print({list, Results1}),
+    print({list, Results2}),
 
-    print({list, [Dir1, Dir2, Dir3, Dir4]}),
+    Lines1 = group(Results1),
+    Lines2 = group(Results2),
+
+
+
+    print({list, Lines1}),
+    print({list, Lines2}),
+
 
     ok.
 
