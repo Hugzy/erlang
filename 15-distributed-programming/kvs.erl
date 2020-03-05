1cf376be79bfaab6477041a3dc8ad29fa28bf1b1
frederik <frede.madsen3@gmail.com>
2020-03-05 14:33:08 +0100

    yay added some bells to the kvs, now missing whistles

--- 15-distributed-programming/kvs.erl
+++ 15-distributed-programming/kvs.erl
@@ -1,18 +1,22 @@
 -module(kvs).
--export([start/0, store/2, lookup/1]).
+-export([start/0, list/0, store/2, lookup/1]).
 
 start() -> register(kvs, spawn(fun() -> loop() end)).
 
+% Functions to call from the client
 store(Key, Value) -> rpc({store, Key, Value}).
-
 lookup(Key) -> rpc({lookup, Key}).
 
+list() -> rpc({list}).
+
+% Middle layer used to call the server
 rpc(Q) ->
     kvs ! {self(), Q},
     receive
         {kvs, Reply} ->
             Reply
     end.
+
 
 loop() ->
     receive
@@ -22,5 +26,12 @@ loop() ->
             loop();
         {From, {lookup, Key}} ->
             From ! {kvs, get(Key)},
+            loop();
+        {From, {keys}} ->
+            From ! {kvs, get_keys()},
+            loop();
+        {From, {list}} ->
+            TempList = [{K, get(K)} || K <- get_keys()],
+            From !{kvs, [{K, V} || {K, {ok, V}} <- TempList]},
             loop()
     end.
